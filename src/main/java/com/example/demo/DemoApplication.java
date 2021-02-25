caf044800c2a4b63ac874777f6151d8c5a931af9
a.sobol <alexandrina.sobol@isa.utm.md>
2021-02-25 16:17:58 +0200

    fisrt commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		//TODO printezi message din properties
+		System.out.println("eeeeeeeeee");
+		SpringApplication.run(DemoApplication.class, args);
+
+	}
+
+}
