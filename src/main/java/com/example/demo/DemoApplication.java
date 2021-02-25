772da139e4ab79f6b60acb2d5a8ae4a57dc81716
Alexandrina Sobol <alexandrinasobol11@gmail.com>
2021-02-25 22:50:57 +0200

    added default goal

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,13 +3,29 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		//TODO printezi message din properties
-		System.out.println("eeeeeeeeee");
 		SpringApplication.run(DemoApplication.class, args);
+
+		//TODO printezi message din properties
+		try (InputStream input = new FileInputStream("config/application.properties")) {
+			var prop = new Properties();
+			prop.load(input);
+
+			System.out.println(prop.getProperty("message"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("eeeeeeeeee");
 
 	}
 
